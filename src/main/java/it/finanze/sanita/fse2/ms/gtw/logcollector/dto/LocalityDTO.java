3a686e5d51aec4021a90657b9c029ff3f6250d2c
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2026-01-09 15:27:16 +0100

    Update LocalityDTO.java

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/logcollector/dto/LocalityDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/logcollector/dto/LocalityDTO.java
@@ -14,6 +14,8 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @Builder
 public class LocalityDTO {
 
+	private static final String STS_OID = "2.16.840.1.113883.2.9.4.1.3";
+	
     @Field("raw_value")
     private String rawValue;
 
@@ -25,10 +27,10 @@ public class LocalityDTO {
 
     /**
      * Decode locality basing on input information.
-     *
+     * 
      * @param locality The field to be decoded.
      * @return A DTO containing locality information.
-     */
+     */ 
     public static LocalityDTO decodeLocality(String locality) {
 
         LocalityDTO out = new LocalityDTO();
@@ -36,6 +38,17 @@ public class LocalityDTO {
 
         if (StringUtils.isEmpty(locality)) {
             return out;
+        }
+
+        String[] parts = locality.split("&");
+
+        if (parts.length < 2) {
+            return out; // non c'è seconda parte
+        }
+
+        String secondPart = parts[1];
+        if (!STS_OID.equals(secondPart)) {
+            return out;  
         }
 
         int lastCaretIndex = locality.lastIndexOf("^^^^");
@@ -46,12 +59,13 @@ public class LocalityDTO {
         String lastToken = locality.substring(lastCaretIndex + 4);
 
         if (lastToken.length() == 12) {
-            out.setAslCode(lastToken.substring(3, 6));       
-            out.setStructure(lastToken.substring(6, 12));    
+            out.setAslCode(lastToken.substring(3, 6));
+            out.setStructure(lastToken.substring(6, 12));
         } else if (lastToken.length() == 6) {
-            out.setAslCode(lastToken.substring(3, 6));       
+            out.setAslCode(lastToken.substring(3, 6));
         }
 
         return out;
     }
+
 }
