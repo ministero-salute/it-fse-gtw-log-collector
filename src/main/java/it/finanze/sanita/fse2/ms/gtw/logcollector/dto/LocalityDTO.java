90312eefaa5da3327162fc9166e87f2f48d33e1e
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2026-01-09 10:45:44 +0100

    fix: Save correct asl code and structure

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/logcollector/dto/LocalityDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/logcollector/dto/LocalityDTO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -33,16 +34,22 @@ public class LocalityDTO {
         LocalityDTO out = new LocalityDTO();
         out.setRawValue(locality);
 
-        if (StringUtils.isNotEmpty(locality)) {
-            if (locality.length() >= 3) {
-                String aslCode = locality.substring(0, 3);
-                out.setAslCode(aslCode);
-            }
+        if (StringUtils.isEmpty(locality)) {
+            return out;
+        }
 
-            if (locality.length() > 3) {
-                String subLocality = locality.substring(3);
-                out.setStructure(subLocality);
-            }
+        int lastCaretIndex = locality.lastIndexOf("^^^^");
+        if (lastCaretIndex == -1) {
+            return out;
+        }
+
+        String lastToken = locality.substring(lastCaretIndex + 4);
+
+        if (lastToken.length() == 12) {
+            out.setAslCode(lastToken.substring(3, 6));       
+            out.setStructure(lastToken.substring(6, 12));    
+        } else if (lastToken.length() == 6) {
+            out.setAslCode(lastToken.substring(3, 6));       
         }
 
         return out;
